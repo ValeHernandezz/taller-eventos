4ebf1c6bb0dd46005067c27ae8c22a3b0c67586d
ValeHernandezz <valehernandezmodino@gmail.com>
2024-08-11 22:01:30 -0300

    Solución problema propagación de eventos en el botón

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
-document.querySelector('div').addEventListener('click', function() {
+document.querySelector('div').addEventListener('click', function () {
   alert('Hola! Soy el div')
+})
+
+document.querySelector('button').addEventListener('click', function (evento) {
+  evento.stopPropagation()  // Detiene la propagación del evento click al div
+  alert('Hola!')
 })
